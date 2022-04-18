004966c0d750715114f59e7b4b757796dee72a3a
dinaraileanu <71759792+dinaraileanu@users.noreply.github.com>
2022-04-18 12:12:19 +0300

    Add files via upload

---
+++ task5_Răileanu.js
@@ -0,0 +1,87 @@
+//sarcina 1
+/*var a = prompt('Introduceti  а', '');
+if (a > 0) {
+  alert( "1" );
+}
+  else if (a < 0) {
+  alert( "-1" );
+}
+  else if (a == 0) {
+  alert( "0" );
+}
+  else {
+  alert( "Error" );
+}*/
+
+//Sarcina 2
+//let numar = ['1', '2', '3', '4', '5'];
+/*numar.forEach(function(entry) {
+    console.log(entry);
+});*/
+/*var index;
+for (index = 0; index < numar.length; ++index) 
+    console.log(numar[index]);*/
+/*for (key in numar) {
+    if (arrayHasOwnIndex(numar, key)) {
+        console.log(numar[key]);
+    }
+}*/
+/*var val;
+for (val of numar) {
+    console.log(val);
+}*/
+/*var it = numar.entries();
+var entry;
+while (!(entry = it.next()).done) {
+    console.log(entry.value[1]);}*/
+
+//sarcina 3
+/*let point =[{x: 1, y:2},{x:3,y:4},{x:null,y:4},{x:3,y:undefined}]
+var ap = point.filter(function(number) {
+    return number < 10;
+  });
+  alert(point);*/
+
+/*const student = {
+    name: "",
+    marks: "",
+   
+};
+const students = [
+    {
+        name: "Primul",
+        marks: [8,10,7,6,9]
+        
+    },
+    {
+        name: "Al doilea",
+        marks: [8,9,8,10,9]
+    },
+    {
+        name: "Al treilea",
+        marks: [8,7,8,9,6]
+    },
+    {
+        name: "Al patrulea",
+        marks: [8,5,6,3,2]
+    },
+    {
+        name: "Al cincelea",
+        marks: [8,2,3,5,4]
+    },
+
+];
+console.log(students);
+let averages = students.map(s => {
+    return {
+      name: s.name,
+      av: s.marks.reduce((a, i) => a + i) / s.marks.length
+    };
+  });
+  console.log(averages);*/
+
+
+
+
+
+
